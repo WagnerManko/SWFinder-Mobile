7f53fbbbc45a596e1457e15f7408b07d0b1f8c89
WagnerManko <wagner.manko@gmail.com>
2020-12-01 17:15:28 -0300

    Aplicação das imagens por Fake Api em Json

--- src/api/googleSearch/index.js
+++
@@ -1,7 +0,0 @@
-  import axios from 'axios';
-
-const googleSearch = axios.create({
-    baseURL: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyBOEVCBzmlAZDi80jK_gPqYxuKiP7BN5CY&cx=49b12604b5af92a0c&q=tatooine'
-});
-
-export default googleSearch;
--- src/components/searchResult/index.js
+++ src/components/searchResult/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Image, Text, TouchableOpacity } from 'react-native';
-import api from '../../api/googleSearch';
+
+import api from '../../api/imgapi/avatars.json';
 
 import style from './style';
 
@@ -9,21 +10,9 @@ function Result (props) {
 
     const navigation = useNavigation();
 
-    async function loadAvatars(){
-      const response = await api.get();
-
-      const resItem = response.data.items[0];
-
-      const avatar = resItem.pagemap.cse_image.cse_image[0].src;
-
-      console.log(avatar);
-    }
-
-    loadAvatars();
-
     return (
         <TouchableOpacity style={style.btnItem}>
-          <Image style={style.avatarItem} source={{uri: 'https://upload.wikimedia.org/wikipedia/pt/thumb/c/cf/LukeTatooine.jpg/200px-LukeTatooine.jpg'}}/>
+          <Image style={style.avatarItem} source={{ uri: api[props.itemMap]}}/>
           <Text style={style.nameItem}>{props.itemMap}</Text>
         </TouchableOpacity>
     );

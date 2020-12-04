a6a4cd72b609ec879935120769c5f996dc6ced89
WagnerManko <wagner.manko@gmail.com>
2020-12-03 22:11:48 -0300

    Codificação e Criação da Page People

--- src/pages/peopleX/index.js
+++ src/pages/peopleX/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
-export default function PeopleX(props){
+import style from './style';
+
+export default function PeopleX(data){
+
+    const avatar = data.route.params.avatar;
     const people = data.route.params.data;
 
-    return <Text>{people.name}</Text>
+    return (
+        <ScrollView style={style.viewBody}>
+            <View style={style.viewPeople}>
+                <Image style={style.peopleImg} source={{uri: avatar}}/>
+                <Text style={style.itemTitle}>{people.name}</Text>
+            </View>
+            <View style={style.viewDetails}>
+                <Text style={style.itemSubTitleDetails}>Information</Text>
+                <Text style={style.itemText}>Height: <Text style={style.itemData}>{people.height}</Text></Text>
+                <Text style={style.itemText}>Mass: <Text style={style.itemData}>{people.mass}</Text></Text>
+                <Text style={style.itemText}>Hair Color: <Text style={style.itemData}>{people.hair_color}</Text></Text>
+                <Text style={style.itemText}>Skin Color: <Text style={style.itemData}>{people.skin_color}</Text></Text>
+                <Text style={style.itemText}>Eye Color: <Text style={style.itemData}>{people.eye_color}</Text></Text>
+                <Text style={style.itemText}>Birth Year: <Text style={style.itemData}>{people.birth_yea}</Text></Text>
+                <Text style={style.itemText}>Gender: <Text style={style.itemData}>{people.gender}</Text></Text>
+            </View>        
+        </ScrollView>
+    )
 }
---
+++ src/pages/peopleX/style.js
@@ -0,0 +1,70 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const fullWidth = Dimensions.get('screen').width;
+const fullHeight = Dimensions.get('screen').height;
+
+const style = StyleSheet.create({
+    viewBody: {
+        flex: 1,
+        backgroundColor: '#1c1e22',
+    },
+
+        viewPeople: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderBottomWidth: 2,
+            borderColor: '#000000',
+            paddingBottom: 20,
+        },
+            peopleImg: {
+                minHeight: fullHeight / 4,
+                minWidth: fullWidth -8,
+                borderWidth: 1,
+                borderColor: '#ffe300',
+                borderRadius: 4,
+            },
+            itemTitle: {
+                fontSize: 18,
+                textAlign: 'center',
+                fontFamily: 'Sarpanch_600SemiBold',
+                color: '#ffe300',
+                textTransform: 'uppercase',
+                marginTop: 20,
+                paddingLeft: 18,
+                paddingRight: 18,
+            },
+            itemSubTitle: {
+                fontSize: 16,
+                textAlign: 'center',
+                fontFamily: 'Sarpanch_500Medium',
+                color: '#ffe300',
+                paddingLeft: 18,
+                paddingRight: 18,
+            },
+
+        viewDetails: {
+            paddingLeft: 20,
+            borderBottomWidth: 1,
+            borderColor: '#000000',
+            paddingBottom: 20,
+        }, 
+            itemSubTitleDetails: {
+                marginTop: 20,
+                marginBottom: 20,
+                fontSize: 16,
+                fontFamily: 'Sarpanch_500Medium',
+                color: '#ffe300',
+                paddingLeft: 18,
+                paddingRight: 18,
+            },
+            itemText: {
+                color: '#ffe300',
+                paddingLeft: 18,
+                paddingRight: 18,
+            },
+            itemData: {
+                color: '#ffffff',
+            },
+});
+
+export default style;
